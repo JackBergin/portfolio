77e73d1833bafe1b6957c90ed7ba2e395c535c0e
JackBergin <jackcbergin@gmail.com>
2025-01-05 15:00:49 -0500

    Adjusted the packagejson and deploy so we can have separate build methods for gh deployment and local test dev

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,12 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  pathPrefix: "/portfolio",
+  pathPrefix: process.env.NODE_ENV === "production" ? "/portfolio" : "",
   siteMetadata: {
     title: `Jack Bergin Portfolio`,
-    siteUrl: `https://jackbergin.github.io/portfolio`
+    siteUrl: process.env.NODE_ENV === "production" 
+      ? `https://jackbergin.github.io/portfolio`
+      : `http://localhost:8000`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
